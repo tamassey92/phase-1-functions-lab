df14c31cc3cecbb498f665e86f5574014166c987
Taylor Massey <tamassey92@gmail.com>
2022-09-01 12:48:14 -0400

    commit tree 290e8e75676e78f10fb79e090ae9d86f808c56d6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (someValue) {
+    if (someValue < 42){
+        return(42 - someValue);
+    }
+    else {
+        return(someValue - 42);
+    }
+}
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+ }
+
+ function distanceTravelledInFeet(start, destination) {
+if (start < destination)
+return(destination - start)*264
+else if (start > destination)
+return(start - destination)*264
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let total = distanceTravelledInFeet(start, destination)
+    if (total <= 400)
+    return 0;
+    else if (total >= 400 && total <= 2000)
+    return (total - 400)*.02;
+    else if (total >= 2000 && total <= 2500)
+    return 25;
+    else if (total > 2500)
+    return "cannot travel that far"
+  }
